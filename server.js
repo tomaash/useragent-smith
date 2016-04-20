8e18b0550cebaf6e9e4b2edcc63b3ef0ac45db49
Tomas Holas <tomas.holas@gmail.com>
2016-04-20 11:22:42 +0200

    qr

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ app.use(logger());
 app.use(serve(__dirname + '/images'));
 
 app.use(function*(next) {
-	this.body = `<h1>Agent Smith says:</h1><h3>${this.req.headers['user-agent']}</h3><img src="agent-smith.gif"/>`;
+	this.body = `<h1>Agent Smith says:</h1>
+	<h3>${this.req.headers['user-agent']}</h3>
+	<img src="http://api.qrserver.com/v1/create-qr-code/?color=000000&amp;bgcolor=FFFFFF&amp;data=https%3A%2F%2Fuseragent-smith.herokuapp.com%2F&amp;qzone=1&amp;margin=0&amp;size=400x400&amp;ecc=L" alt="qr code" />
+	<img src="agent-smith.gif"/>
+	`;
 });
 
 var port = process.env.PORT || 3001;
